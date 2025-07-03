41eeb2d514a5d7a87749e88ffd93aa218bb01660
waylon.chen <waylon.chen@quectel.com>
2025-07-03 17:43:54 +0800

    :fix 修改

---
+++ utils/config.js
@@ -0,0 +1,30 @@
+export const VERSIOIN = '1.3.9'
+export const updateTime = '2025-06-02'
+export const EXPIRE_TIME = 60 * 60 * 1
+export const ENTITY = 'William'
+export const APP_NAME = 'WilliamPlayer'
+export const TOKEN_KEY = `${APP_NAME}_ACCESS_TOKEN`
+export const USER_KEY = `${APP_NAME}_USER_INFO`
+export const EXPIRE_TIME_KEY = `${APP_NAME}_EXPIRE_TIME`
+export const OPEN_ID = `${APP_NAME}_OPEN_ID`
+export const USER_ID = `${APP_NAME}_USER_ID`
+export const BASE_URL = `http://127.0.0.1:4040/ruoyi`
+let IMG_DOMAIN = ''
+// #ifdef APP-PLUS
+IMG_DOMAIN = `https://image.tmdb.org`
+export { IMG_DOMAIN }
+// #endif
+
+// #ifdef H5
+IMG_DOMAIN = `https://media.themoviedb.org`
+export { IMG_DOMAIN }
+// #endif
+export const Folder189Url = `https://cloud.189.cn/api/open/file/listFiles.action?iconOption=5&orderBy=filename&descending=false`
+export const Video189Url = `https://cloud.189.cn/api/portal/getNewVlcVideoPlayUrl.action`
+export const User189Url = `https://cloud.189.cn/api/open/user/getUserInfoForPortal.action`
+export const Download189Url = `https://cloud.189.cn/api/open/file/getFileDownloadUrl.action`
+export const QuarkFolderUrl = `https://drive-pc.quark.cn/1/clouddrive/file/sort?pr=ucpro&fr=pc&uc_param_str=&_fetch_total=1&_sort=file_type:asc,file_name:asc`
+export const QuarkVideoUrl = `https://drive-pc.quark.cn/1/clouddrive/file/download?pr=ucpro&uc_param_str=&fr=pc&sys=win32&ve=3.2.8`
+export const QuarkResolutionUrl = `https://drive-pc.quark.cn/1/clouddrive/file/v2/play?pr=ucpro&uc_param_str=&fr=pc`
+export const QuarkUserUrl = `https://pan.quark.cn/account/info?fr=pc&platform=pc`
+
