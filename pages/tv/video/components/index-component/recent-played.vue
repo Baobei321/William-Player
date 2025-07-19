69e0615e884d535c9d8a122993200c91e13002df
Chen <1075005842@qq.com>
2025-07-19 08:08:34 +0800

    fix(location):修改最近播放的焦点

--- pages/tv/video/components/index-component/recent-played.vue
+++ pages/tv/video/components/index-component/recent-played.vue
@@ -9,20 +9,25 @@
       </div>
     </div>
     <div class="recent-played-list">
-      <scroll-view class="recent-played-list-scroll" :scroll-x="true" style="width: 100%" :enhanced="true" :showScrollbar="false">
+      <scroll-view class="recent-played-list-scroll" :scroll-x="true" style="width: 100%" :enhanced="true"
+        :showScrollbar="false">
         <div class="recent-played-list-movie">
-          <div class="recent-played-list-movie__item" v-for="item in scrollData" :key="item.name" @click="toVideoPlayer(item)">
+          <div class="recent-played-list-movie__item" v-for="item in scrollData" :key="item.name"
+            @click="toVideoPlayer(item)">
             <div class="recent-played-list-movie__item-img">
               <image :src="item.poster" style="width: 100%;height: 100%;position: static;" mode="aspectFill"></image>
               <span style="color: red;font-size: 30px;">
                 {{ item.loadImg }}
               </span>
               <image :src="playVideoButton" class="img-button" />
-              <span class="img-runtime">{{ handleSecond(item.initialTime)+'/'+ item.runtime }}</span>
-              <div class="img-process" :style="{width:Number(item.initialTime)/(Number(parseTime(item.runtime))*0.6)+'%'}"></div>
+              <span class="img-runtime">{{ handleSecond(item.initialTime) + '/' + item.runtime }}</span>
+              <div class="img-process"
+                :style="{ width: Number(item.initialTime) / (Number(parseTime(item.runtime)) * 0.6) + '%' }"></div>
             </div>
-            <span class="recent-played-list-movie__item-name" v-if="item.type=='movie'">{{ handleSeasonName(removeExtension(item.name)) }}</span>
-            <span class="recent-played-list-movie__item-name" v-if="item.type=='tv'">{{ removeExtension(`${item.titlePlay} 第${item.ji}集 ${item.title}`) }}</span>
+            <span class="recent-played-list-movie__item-name" v-if="item.type == 'movie'">{{
+              handleSeasonName(removeExtension(item.name)) }}</span>
+            <span class="recent-played-list-movie__item-name" v-if="item.type == 'tv'">{{
+              removeExtension(`${item.titlePlay} 第${item.ji}集 ${item.title}`) }}</span>
           </div>
         </div>
       </scroll-view>
@@ -39,10 +44,13 @@ import emptyBg from "@/static/empty_bg.png";
 import { toStringfy } from "@/pages/mobile/mine/common";
 
 const props = defineProps({
+  isFocus: { type: Boolean, default: true },
   listData: { type: Array, default: [] },
 });
 
 const scrollData = ref([]);
+const tabIndex = ref(-1);
+
 
 const selectType = ref({});
 const selectMedia = ref({});
@@ -146,6 +154,50 @@ const toVideoAll = () => {
     url: `/pages/mobile/video/video-all?title=最近观看`,
   });
 };
+//获取上下的组件
+const getUpDown = (direction) => {
+  if (direction === "up") {
+    return "starRecommend";
+  } else if (direction === 'down') {
+    let localMovieTvData = uni.getStorageSync("localMovieTvData");
+    if (localMovieTvData?.movie?.length) {
+      return "hxMovie";
+    }
+    if (localMovieTvData?.tv?.length) {
+      return "hxTv";
+    }
+  }
+}
+
+let nowTime = 0;
+const evtMove = (keyCode) => {
+  if (keyCode === "KeyRight") {
+    if (tabIndex.value != props.listData.length - 1) {
+      tabIndex.value++;
+    }
+  } else if (keyCode === "KeyLeft") {
+    if (tabIndex.value > 0) {
+      tabIndex.value--;
+    }
+  } else if (keyCode === "KeyUp") {
+    emits("setFocus", getUpDown("up"), 'KeyUp');
+  } else if (keyCode === "KeyDown") {
+    emits("setFocus", getUpDown("down"), 'KeyDown');
+  } else if (keyCode === 'KeyEnter') {
+
+  }
+  let time = Date.now();
+  if (time - nowTime > 300) {
+    if (tabIndex.value >= 3) {
+      scrollIntoView.value = "name" + (tabIndex.value - 1);
+    } else {
+      scrollIntoView.value = "name1";
+    }
+  } else {
+    setScrollIntoView();
+  }
+  nowTime = time;
+};
 
 onShow(() => {
   judgeSelect();
@@ -165,6 +217,21 @@ watch(
   },
   { immediate: true, deep: true }
 );
+
+watch(
+  () => props.isFocus,
+  (val) => {
+    if (val) {
+      tabIndex.value = 0;
+    } else {
+      tabIndex.value = -1;
+    }
+  }
+);
+
+defineExpose({
+  evtMove
+})
 </script>
 
 <style lang="scss" scoped>
@@ -172,6 +239,7 @@ watch(
   width: 100%;
   margin: 50rpx 0;
   padding: 0 80rpx;
+
   .recent-played-title {
     display: flex;
     align-items: center;
@@ -198,6 +266,7 @@ watch(
       }
     }
   }
+
   .recent-played-list {
     margin-top: 24rpx;
 
@@ -209,9 +278,11 @@ watch(
         display: flex;
         flex-wrap: nowrap;
         align-items: center;
+
         .recent-played-list-movie__item {
           margin-left: 24rpx;
           flex: 0 0 520rpx;
+
           .recent-played-list-movie__item-img {
             background-position: center;
             background-repeat: no-repeat;
@@ -221,6 +292,7 @@ watch(
             border-radius: 20rpx;
             position: relative;
             overflow: hidden;
+
             .img-button {
               width: 80rpx;
               height: 80rpx;
@@ -230,6 +302,7 @@ watch(
               transform: translate(-50%, -50%);
               object-fit: cover;
             }
+
             .img-runtime {
               position: absolute;
               right: 10rpx;
@@ -240,6 +313,7 @@ watch(
               border-radius: 8rpx;
               padding: 4rpx 8rpx;
             }
+
             .img-process {
               height: 7rpx;
               background: #ff6701;
@@ -248,11 +322,13 @@ watch(
               left: 0;
             }
           }
+
           &-name {
             font-size: 28rpx;
             font-weight: bold;
             color: #000;
           }
+
           &:first-child {
             margin-left: 0;
           }
@@ -261,18 +337,21 @@ watch(
     }
   }
 }
+
 @media (prefers-color-scheme: dark) {
   .recent-played {
     .recent-played-title {
       .recent-played-title-left {
         color: #fff;
       }
+
       .recent-played-title-right {
         span {
           color: rgb(154, 154, 154);
         }
       }
     }
+
     .recent-played-list {
       .recent-played-list-scroll {
         .recent-played-list-movie {
